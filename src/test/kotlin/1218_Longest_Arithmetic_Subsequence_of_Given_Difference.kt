33e76134ef4831332afbcea8a0b075e259e9eff3
Fernando Diaz <fediazgon@gmail.com>
2023-07-14 13:56:22 +0200

    Add '1218. Longest Arithmetic Subsequence of Given Difference'

---
+++ src/test/kotlin/1218_Longest_Arithmetic_Subsequence_of_Given_Difference.kt
@@ -0,0 +1,43 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class LongestArithmeticSubsequenceOfGivenDifference1218Test {
+
+    @Test
+    fun example1() {
+        assertEquals(
+            4, LongestArithmeticSubsequenceOfGivenDifference1218.longestSubsequence(
+                intArrayOf(1, 2, 3, 4), 1
+            )
+        )
+    }
+
+    @Test
+    fun example2() {
+        assertEquals(
+            1, LongestArithmeticSubsequenceOfGivenDifference1218.longestSubsequence(
+                intArrayOf(1, 3, 5, 7), 1
+            )
+        )
+    }
+
+    @Test
+    fun example3() {
+        assertEquals(
+            4, LongestArithmeticSubsequenceOfGivenDifference1218.longestSubsequence(
+                intArrayOf(1, 5, 7, 8, 5, 3, 4, 2, 1), -2
+            )
+        )
+    }
+
+    @Test
+    fun example4() {
+        assertEquals(
+            2, LongestArithmeticSubsequenceOfGivenDifference1218.longestSubsequence(
+                intArrayOf(4,12,10,0,-2,7,-8,9,-9,-12,-12,8,8), 0
+            )
+        )
+    }
+
+}
+
