609d756ac576f9982c9181b1bc401d5f9d567d47
Fernando Diaz <fediazgon@gmail.com>
2023-07-14 19:33:19 +0200

    Add '80. Remove Duplicates from Sorted Array II'

---
+++ src/main/kotlin/80_Remove_Duplicates_from_Sorted_Array_II.kt
@@ -0,0 +1,24 @@
+class RemoveDuplicatesFromSortedArrayII80 {
+
+    companion object {
+
+        fun removeDuplicates(nums: IntArray): Int {
+            if (nums.size < 3) {
+                return nums.size
+            }
+            var prev = nums[0]
+            var count = 1
+            var insertIdx = 1
+            for (n in nums.drop(1)) {
+                if (n != prev || count < 2)  {
+                    nums[insertIdx++] = n
+                    count = if (n != prev) 1 else count + 1
+                    prev = n
+                }
+            }
+            return insertIdx
+        }
+
+    }
+
+}
