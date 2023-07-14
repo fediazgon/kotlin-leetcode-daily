60e93a137d6181917026bc556f134327ef9e8021
Fernando Diaz <fediazgon@gmail.com>
2023-07-14 09:44:32 +0200

    Improve solution for '624. Maximum Distance in Arrays'

--- src/main/kotlin/624_Maximum_Distance_Arrays.kt
+++ src/main/kotlin/624_Maximum_Distance_Arrays.kt
@@ -1,41 +1,21 @@
 import kotlin.math.abs
 
-// TODO: There is a better Single Scan version
 class MaximumDistanceArrays624 {
 
     companion object {
 
         fun maxDistance(arrays: List<List<Int>>): Int {
-            if (arrays.size == 1) {
-                return 0
+            var maxDistance = 0
+            var min = arrays[0].first()
+            var max = arrays[0].last()
+            for (l in arrays.drop(1)) {
+                val first = l.first()
+                val last = l.last()
+                maxDistance = maxOf(maxDistance, abs(max - first), abs(last - min))
+                min = minOf(min, first)
+                max = maxOf(max , last)
             }
-            var minArr = -1
-            var maxArr = -1
-            var min = Int.MAX_VALUE
-            var secondMin = Int.MAX_VALUE
-            var max = Int.MIN_VALUE
-            var secondMax = Int.MIN_VALUE
-            for ((i, arr) in arrays.withIndex()) {
-                if (arr.first() < min) {
-                    minArr = i
-                    secondMin = min
-                    min = arr.first()
-                } else if (arr.first() < secondMin) {
-                    secondMin = arr.first()
-                }
-                if (arr.last() > max) {
-                    maxArr = i
-                    secondMax = max
-                    max = arr.last()
-                } else if (arr.last() > secondMax) {
-                    secondMax = arr.last()
-                }
-            }
-            return if (minArr != maxArr) {
-                abs(max - min)
-            } else {
-                maxOf( abs(max - secondMin), (secondMax - min))
-            }
+            return maxDistance
         }
 
     }
