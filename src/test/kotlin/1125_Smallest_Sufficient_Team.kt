c2b737ca7230f245059b079e5fd1f77b92072770
Fernando Diaz <fediazgon@gmail.com>
2023-07-16 23:19:56 +0200

    Add '1125. Smallest Sufficient Team'

---
+++ src/test/kotlin/1125_Smallest_Sufficient_Team.kt
@@ -0,0 +1,29 @@
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class SmallestSufficientTeam1125Test {
+
+  @Test
+  fun example1() {
+    assertContentEquals(
+        intArrayOf(0, 2),
+        SmallestSufficientTeam1125.smallestSufficientTeam(
+            arrayOf("java", "nodejs", "reactjs"),
+            listOf(listOf("java"), listOf("nodejs"), listOf("nodejs", "reactjs"))))
+  }
+
+  @Test
+  fun example2() {
+    assertContentEquals(
+        intArrayOf(1, 2),
+        SmallestSufficientTeam1125.smallestSufficientTeam(
+            arrayOf("algorithms", "math", "java", "reactjs", "csharp", "aws"),
+            listOf(
+                listOf("algorithms", "math", "java"),
+                listOf("algorithms", "math", "reactjs"),
+                listOf("java", "csharp", "aws"),
+                listOf("reactjs", "csharp"),
+                listOf("csharp", "math"),
+                listOf("aws", "java"))))
+  }
+}
