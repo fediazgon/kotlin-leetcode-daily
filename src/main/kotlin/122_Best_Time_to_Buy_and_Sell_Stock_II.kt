8d226b63a81d1b3f0d0431cdb95f4667b2c5d529
Fernando Diaz <fediazgon@gmail.com>
2023-07-25 14:44:40 +0200

    Add '122. Best Time to Buy and Sell Stock II'

--- src/main/kotlin/122_Best_Time_to_Buy_and_Sell_Stock_II.kt
+++ src/main/kotlin/122_Best_Time_to_Buy_and_Sell_Stock_II.kt
@@ -1,3 +1,18 @@
 fun maxProfitII(prices: IntArray): Int {
-    return 0
+  var totalProfit = 0
+  var localMin = Integer.MAX_VALUE
+  for (i in prices.indices) {
+    val p = prices[i]
+    if (p < localMin) {
+      localMin = p
+    } else if (isLocalMax(prices, i) && i != 0) {
+      totalProfit += p - localMin
+      localMin = Integer.MAX_VALUE
+    }
+  }
+  return totalProfit
 }
+
+private fun isLocalMax(prices: IntArray, idx: Int): Boolean =
+    (idx == 0 || prices[idx - 1] <= prices[idx]) &&
+        (idx == prices.size - 1 || prices[idx + 1] < prices[idx])
--- src/test/kotlin/122_Best_Time_to_Buy_and_Sell_Stock_II.kt
+++ src/test/kotlin/122_Best_Time_to_Buy_and_Sell_Stock_II.kt
@@ -3,20 +3,33 @@ import kotlin.test.assertEquals
 
 class BestTimeToBuyAndSellStock122Test {
 
-    @Test
-    fun example1() {
-        assertEquals(7, maxProfitII(intArrayOf(7,1,5,3,6,4)))
-    }
+  @Test
+  fun example1() {
+    assertEquals(7, maxProfitII(intArrayOf(7, 1, 5, 3, 6, 4)))
+  }
 
+  @Test
+  fun example2() {
+    assertEquals(4, maxProfitII(intArrayOf(1, 2, 3, 4, 5)))
+  }
 
-    @Test
-    fun example2() {
-        assertEquals(4, maxProfitII(intArrayOf(1,2,3,4,5)))
-    }
+  @Test
+  fun example3() {
+    assertEquals(0, maxProfitII(intArrayOf(7, 6, 4, 3, 1)))
+  }
 
+  @Test
+  fun example4() {
+    assertEquals(10, maxProfitII(intArrayOf(10, 5, 0, 10)))
+  }
 
-    @Test
-    fun example3() {
-        assertEquals(0, maxProfitII(intArrayOf(7,6,4,3,1)))
-    }
+  @Test
+  fun example5() {
+    assertEquals(5, maxProfitII(intArrayOf(0, 5, 5, 0)))
+  }
+
+  @Test
+  fun example6() {
+    assertEquals(5, maxProfitII(intArrayOf(5, 10, 10, 5, 5, 5, 0)))
+  }
 }
