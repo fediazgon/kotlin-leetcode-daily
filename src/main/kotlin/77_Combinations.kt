eca572554c0ee977dca23e61c712dc62378dbb12
Fernando Diaz <fediazgon@gmail.com>
2023-08-01 15:36:01 +0200

    Add iterative method for: '77. Combinations'

--- src/main/kotlin/77_Combinations.kt
+++ src/main/kotlin/77_Combinations.kt
@@ -1,16 +1,43 @@
-fun combine(n: Int, k: Int): List<List<Int>> {
+fun combine(n: Int, k: Int): List<List<Int>> = combineIter(n, k)
+
+private fun combineRec(n: Int, k: Int): List<List<Int>> {
   val comb = mutableListOf<List<Int>>()
-  combine(comb, mutableListOf(), 1, n, k)
+  combineRec(comb, mutableListOf(), 1, n, k)
   return comb
 }
 
-private fun combine(comb: MutableList<List<Int>>, curr: MutableList<Int>, i: Int, n: Int, k: Int) {
+private fun combineRec(
+    comb: MutableList<List<Int>>,
+    curr: MutableList<Int>,
+    i: Int,
+    n: Int,
+    k: Int
+) {
   if (curr.size == k) {
     comb.add(curr.toList())
   } else if (i <= n) {
     curr.add(i)
-    combine(comb, curr, i + 1, n, k)
+    combineRec(comb, curr, i + 1, n, k)
     curr.removeAt(curr.lastIndex)
-    combine(comb, curr, i + 1, n, k)
+    combineRec(comb, curr, i + 1, n, k)
   }
+}
+
+private fun combineIter(n: Int, k: Int): List<List<Int>> {
+  var combCurr: List<LinkedHashSet<Int>> = (1..n).map { LinkedHashSet(listOf(it)) }.toList()
+  for (s in 2..k) {
+    val tmp: MutableList<LinkedHashSet<Int>> = mutableListOf()
+    for (c in combCurr) {
+      for (i in 1..n) {
+        if (!c.contains(i)) {
+          val next = c + i
+          if (!tmp.contains(next)) {
+            tmp.add(next as LinkedHashSet<Int>)
+          }
+        }
+      }
+    }
+    combCurr = tmp
+  }
+  return combCurr.map { it.toList() }
 }
